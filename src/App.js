113e4a27e6654e023858557dad272f1996ec25aa
Akshit <akshit@Akshits-iMac.local>
2022-07-28 07:51:59 -0400

    create textarea

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import './App.css';
-import Button from './components/Button';
+import "./App.css"
+import Button from "./components/Button"
+import Textarea from "./components/Text/text"
 
 function App() {
-
   const click = () => {
-    console.log("clicked");
+    console.log("clicked")
   }
 
   return (
     <div className="App">
       <h1>Hello</h1>
-      <Button colour="primary" borderRadius onClick={click}>Test</Button>
+      <Button colour="primary" borderRadius onClick={click}>
+        Test
+      </Button>
+      <Textarea type="text" label="Name" id="123" />
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
