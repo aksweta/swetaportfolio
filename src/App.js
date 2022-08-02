c3e2873d660e187af58cb90c8b5c97042f01bc09
Akshit <akshit@Akshits-iMac.local>
2022-08-02 07:24:51 -0400

    create link

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import Button from "./components/Button"
 import Heading from "./components/Heading"
 import Textarea from "./components/Text/text"
 import Text from "./components/Paragraph"
+import Link from "./components/Link"
 
 function App() {
   const click = () => {
+    console.log("clicked")
+  }
+
+  const clicked = () => {
     console.log("clicked")
   }
 
@@ -18,6 +23,7 @@ function App() {
       <Textarea type="text" label="Name" id="123" />
       <Heading type="h6" colour="red">sweta godi</Heading>
       <Text type="p" colour="black" italic>akshit Gondo</Text>
+      <Link href="https://mail.google.com/mail/u/2/?ogbl#inbox" onClick={clicked}>Gmail</Link>
     </div>
   )
 }
